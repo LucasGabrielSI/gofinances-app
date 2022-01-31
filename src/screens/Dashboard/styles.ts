5b76e13e7d5263adcb5dc54315fa7e9030c3fa0e
Lucas Bento <lucasiguape16@gmail.com>
2022-01-31 18:49:12 -0300

    Adding project font and your configurations

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,13 +2,14 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   background-color: ${ ({ theme }) => theme.colors.background }
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  color: purple;
+  color: ${ ({ theme }) => theme.colors.title };
+  font-family: ${({ theme }) => theme.fonts.bold }
 `;
