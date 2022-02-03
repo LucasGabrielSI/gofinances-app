fae3f73c71cf72423070b599a52b0bc48d8bc0ff
Lucas Bento <lucasiguape16@gmail.com>
2022-02-03 18:22:21 -0300

    Continuing development to HighlightCards

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -4,20 +4,40 @@ import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View`
   padding: 19px 23px;
-  border-radius: 5 px;
+  margin-right: 16px;
+  border-radius: 5px;
   width: ${ RFValue(300) }px;
-  padding-bottom: ${ RFValue(42) };
+  padding-bottom: ${ RFValue(42) }px;
   background-color: ${ ({ theme }) => theme.colors.shape };
 `;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: ${ RFValue(14)}px;
+  font-family: ${ ({ theme }) => theme.fonts.regular };
+  color: ${ ({ theme }) => theme.colors.text_dark };
+`;
 
-export const Icon = styled(Feather)``;
+export const Icon = styled(Feather)`
+  font-size: ${ RFValue(40) }px;
+  color: ${ ({ theme }) => theme.colors.text_dark };
+`;
 
 export const Footer = styled.View``;
 
-export const Amount = styled.Text``;
+export const Amount = styled.Text`
+  margin-top: 38px;
+  font-size: ${ RFValue(32)}px;
+  color: ${ ({ theme }) => theme.colors.text_dark };
+  font-family: ${ ({ theme }) => theme.fonts.medium };
+`;
 
-export const LastTransaction = styled.Text``;
+export const LastTransaction = styled.Text`
+  font-size: ${ RFValue(12)}px;
+  color: ${ ({ theme }) => theme.colors.text };
+  font-family: ${ ({ theme }) => theme.fonts.regular };
+`;
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -55,3 +55,5 @@ export const Icon = styled(Feather)`
   color: ${ ({ theme }) => theme.colors.secondary };
   font-size: ${ RFValue(24) }px;
 `;
+
+export const HighlightCards = styled.ScrollView``;
