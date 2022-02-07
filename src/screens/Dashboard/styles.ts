61e1a21e130b8c64f25f076122dc39ec5350d62b
Lucas Bento <lucasiguape16@gmail.com>
2022-02-07 19:16:05 -0300

    Finalizing Highlight cards

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -9,7 +10,7 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   width: 100%;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
   justify-content: center;
   height: ${ RFPercentage(42) }px;
@@ -22,6 +23,7 @@ export const UserWrapper = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  margin-top: ${ getStatusBarHeight() + RFValue(28) }px;
 `;
 
 export const UserInfo = styled.View`
@@ -56,4 +58,12 @@ export const Icon = styled(Feather)`
   font-size: ${ RFValue(24) }px;
 `;
 
-export const HighlightCards = styled.ScrollView``;
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true, 
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 }
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${ RFPercentage(20) }px;
+`;
